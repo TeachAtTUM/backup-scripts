bec11f7c558d68eb96c670cfb986d691dac5e73f
Lukas Tenbrink <lukastenbrink@googlemail.com>
2017-08-22 09:36:16 +0200

    Add mysql import script

---
+++ import-mysql.sh
@@ -0,0 +1,32 @@
+if (( $# != 1 ))
+then
+  echo "Please supply a backup directory"
+  exit 1
+fi
+
+MYSQL_USER="root"
+
+DUMP_DIR=$1
+if [ ! -f $DUMP_DIR ]; then
+    echo "Backup directory not found!"
+fi
+
+# edxapp
+mysql -u "$MYSQL_USER" edxapp < $DUMP_DIR/edxapp.sql
+mysql -u "$MYSQL_USER" edxapp_csmh < $DUMP_DIR/edxapp_csmh.sql
+
+mysql -u "$MYSQL_USER" dashboard < $DUMP_DIR/dashboard.sql
+
+mysql -u "$MYSQL_USER" reports < $DUMP_DIR/reports.sql
+
+mysql -u "$MYSQL_USER" xqueue < $DUMP_DIR/xqueue.sql
+	
+# E-Commerce
+mysql -u "$MYSQL_USER" ecommerce < $DUMP_DIR/ecommerce.sql
+
+# MySQL Users
+mysql -u "$MYSQL_USER" mysql < $DUMP_DIR/mysql.sql
+
+# Analytics
+mysql -u "$MYSQL_USER" analytics < $DUMP_DIR/analytics.sql
+mysql -u "$MYSQL_USER" analytics-api < $DUMP_DIR/analytics-api.sql
