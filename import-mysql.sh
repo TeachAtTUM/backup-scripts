54464b6256ebde7240497725707e35fcb8a757e8
Lukas Tenbrink <lukastenbrink@googlemail.com>
2017-08-22 10:37:06 +0200

    Create databases when importing

--- import-mysql.sh
+++ import-mysql.sh
@@ -13,21 +13,29 @@ if [ ! -d $DUMP_DIR ]; then
 fi
 
 # edxapp
+print "CREATE SCHEMA edxapp" | mysql -u "$MYSQL_USER" 
 mysql -u "$MYSQL_USER" edxapp < $DUMP_DIR/edxapp.sql
+print "CREATE SCHEMA edxapp_csmh" | mysql -u "$MYSQL_USER" 
 mysql -u "$MYSQL_USER" edxapp_csmh < $DUMP_DIR/edxapp_csmh.sql
 
+print "CREATE SCHEMA dashboard" | mysql -u "$MYSQL_USER" 
 mysql -u "$MYSQL_USER" dashboard < $DUMP_DIR/dashboard.sql
 
+print "CREATE SCHEMA reports" | mysql -u "$MYSQL_USER" 
 mysql -u "$MYSQL_USER" reports < $DUMP_DIR/reports.sql
 
+print "CREATE SCHEMA xqueue" | mysql -u "$MYSQL_USER" 
 mysql -u "$MYSQL_USER" xqueue < $DUMP_DIR/xqueue.sql
 	
 # E-Commerce
+print "CREATE SCHEMA ecommerce" | mysql -u "$MYSQL_USER" 
 mysql -u "$MYSQL_USER" ecommerce < $DUMP_DIR/ecommerce.sql
 
 # MySQL Users
 mysql -u "$MYSQL_USER" mysql < $DUMP_DIR/mysql.sql
 
 # Analytics
+print "CREATE SCHEMA analytics" | mysql -u "$MYSQL_USER" 
 mysql -u "$MYSQL_USER" analytics < $DUMP_DIR/analytics.sql
+print "CREATE SCHEMA analytics-api" | mysql -u "$MYSQL_USER" 
 mysql -u "$MYSQL_USER" analytics-api < $DUMP_DIR/analytics-api.sql
