0465bd0582bb3c5ed40ce747adac4b21af5345bd
Lukas Tenbrink <lukastenbrink@googlemail.com>
2018-10-24 17:30:52 +0200

    Change up the scripts for mediadrop server

--- dump-mysql.sh
+++ dump-mysql.sh
@@ -4,31 +4,15 @@ then
   exit 1
 fi
 
-MYSQL_USER="backup"
+MYSQL_USER="root"
 
 DUMP_DIR=$1
 mkdir $DUMP_DIR
 
-# edxapp
-mysqldump -u "$MYSQL_USER" edxapp > $DUMP_DIR/edxapp.sql
-mysqldump -u "$MYSQL_USER" edxapp_csmh > $DUMP_DIR/edxapp_csmh.sql
 
-mysqldump -u "$MYSQL_USER" dashboard > $DUMP_DIR/dashboard.sql
-
-mysqldump -u "$MYSQL_USER" reports > $DUMP_DIR/reports.sql
-
-mysqldump -u "$MYSQL_USER" xqueue > $DUMP_DIR/xqueue.sql
-	
-# E-Commerce
-mysqldump -u "$MYSQL_USER" ecommerce > $DUMP_DIR/ecommerce.sql
+# mediacore
+mysqldump -u "$MYSQL_USER" mediacore > $DUMP_DIR/mediacore.sql
 
 # MySQL Users
-mysqldump -u "$MYSQL_USER" mysql > $DUMP_DIR/mysql.sql
+mysqldump -u "$MYSQL_USER" --events mysql > $DUMP_DIR/mysql.sql
 
-# Analytics
-mysqldump -u "$MYSQL_USER" analytics > $DUMP_DIR/analytics.sql
-mysqldump -u "$MYSQL_USER" analytics-api > $DUMP_DIR/analytics-api.sql
-
-
-# Mongo
-mongodump -o $DUMP_DIR/mongo
--- export-all.sh
+++ export-all.sh
@@ -10,5 +10,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir $BACKUP_DIR
 rm -rf $BACKUP_DIR/*
 
-$SCRIPT_DIR/export-courses.sh $BACKUP_DIR/courses
 $SCRIPT_DIR/dump-mysql.sh $BACKUP_DIR/mysql-dumps
+
